739f3996d10f0e08d1fb5b476570edf12e3e50b1
llllera <144013057+llllera@users.noreply.github.com>
2024-12-02 00:44:35 +0300

    Delete форма на сервере directory

--- форма на сервере/create_CGI.py
+++
@@ -1,6 +0,0 @@
-from http.server import HTTPServer
-from http.server import CGIHTTPRequestHandler
-
-server_address = ("localhost", 8000) #иногда хочет пустые кавычки
-http_server = HTTPServer(server_address, CGIHTTPRequestHandler)
-http_server.serve_forever()
